30928779dee7b5a29ef8a8365d892b4ea5b70c23
Pablo Strapasson <pablostrapasson98@gmail.com>
2024-04-01 11:28:08 -0300

    gestão de usuarios conectados a um documento

--- servidor/utils/conexoesDocumentos.js
+++ servidor/utils/conexoesDocumentos.js
@@ -1,7 +1,24 @@
-let conexoesDocumentos = []
+const conexoesDocumentos = []
+
+function encontrarConexao(nomeDocumento, usuario){
+    return conexoesDocumentos.find((conexao) => {
+        return (conexao.nomeDocumento === nomeDocumento && conexao.usuario === usuario);
+    });
+}
 
 function adicionarConexao(conexao){
     conexoesDocumentos.push(conexao);
+}
+
+function removerConexao(nomeDocumento, usuario){
+    const index = conexoesDocumentos.findIndex((conexao) => {
+        return (conexao.nomeDocumento === nomeDocumento && conexao.usuario === usuario);
+    });
+
+    if(index !== -1){
+        conexoesDocumentos.splice(index, 1);
+    }
+    console.log(conexoesDocumentos);
 }
 
 function obterUsuariosDocumento(nomeDocumento){
@@ -9,4 +26,4 @@ function obterUsuariosDocumento(nomeDocumento){
         .filter((conexao) => conexao.nomeDocumento === nomeDocumento)
         .map((conexao) => conexao.usuario);
 }
-export { adicionarConexao, obterUsuariosDocumento }
+export { adicionarConexao, obterUsuariosDocumento, removerConexao, encontrarConexao }
